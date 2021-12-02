992d8fc75b5a2e59f650dcff67c8434018210a43
Leo V <virolainen.leo@gmail.com>
2021-12-02 12:59:25 +0200

    ship targeting fix

--- Assets/Scripts/TargetManager.cs
+++ Assets/Scripts/TargetManager.cs
@@ -12,7 +12,7 @@ public class TargetManager : MonoBehaviour {
     public KeyCode seekTargetKey;
 
     //LOCAL VARIABLES
-    public GameObject[] enemyTargets;
+    public List<GameObject> enemyTargets;
     public bool CloseEnoughToFire;
     public float distanceToTarget;
 
@@ -41,7 +41,11 @@ public class TargetManager : MonoBehaviour {
     }
 
     private void Update() {
-        if (waitingToScoutAgain == false || myCannonScript.targetedGO.GetComponentInParent<CannonCrew>().stationHasBeenWiped == true) {
+        if (waitingToScoutAgain == false) { 
+            StartCoroutine("FindBestTarget");
+        }
+        if (myCannonScript.targetedGO.GetComponentInParent<CannonCrew>().stationHasBeenWiped == true) {
+            distanceToTarget = 99999;
             StartCoroutine("FindBestTarget");
         }
         //check if close enough to fire
@@ -54,16 +58,29 @@ public class TargetManager : MonoBehaviour {
 
     IEnumerator FindBestTarget() { //add available PCannons to array --- measure new distance to first cannon in array
         waitingToScoutAgain = true;
-        enemyTargets = GameObject.FindGameObjectsWithTag("PlayerCannon");
+        enemyTargets = new List<GameObject>(GameObject.FindGameObjectsWithTag("PlayerCannon"));
 
-        for (int i = 0; i < enemyTargets.Length; ++i) { //Go through the list of previously found cannon --- Find nearest cannon and set as target
-            float nextCannonDist = Vector3.Distance(gameObject.transform.position, enemyTargets[i].transform.position);
+        
 
-            if (nextCannonDist < distanceToTarget)  {
-                if (enemyTargets[i].GetComponentInParent<CannonCrew>().stationHasBeenWiped == false) {//check if new target candidate is intact
+        List<GameObject> activeTargets = new List<GameObject>();
+
+        foreach (GameObject target in enemyTargets) {
+            if (target.GetComponentInParent<CannonCrew>().stationHasBeenWiped == false) {
+                activeTargets.Add(target);
+            }
+        }
+        print("SEARCHING FOR NEW SHIT");
+        foreach (GameObject target in activeTargets) {
+            print(target);
+        }
+        for (int i = 0; i < activeTargets.Count; ++i) { //Go through the list of previously found cannon --- Find nearest cannon and set as target
+            float nextCannonDist = Vector3.Distance(gameObject.transform.position, activeTargets[i].transform.position);
+
+            if (nextCannonDist < distanceToTarget) {
+                if (activeTargets[i].GetComponentInParent<CannonCrew>().stationHasBeenWiped == false) {//check if new target candidate is intact
                     distanceToTarget = nextCannonDist;
                     print(distanceToTarget);
-                    myCannonScript.targetedGO = enemyTargets[i];
+                    myCannonScript.targetedGO = activeTargets[i];
                 }
             }
         } //find HP script for new target cannon
